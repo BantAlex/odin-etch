5d175699a7dd75e5b68c164626eaadda4b1f9d17
Alex Bandis <bantalex3@gmail.com>
2024-05-09 17:53:54 +0300

    Make the slider functional. Grid creator not working as intented

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 //DOM selectors and variables
 const board = document.querySelector('.board');
-const clearButton = document.querySelector('.clear-button');
+const clearButton = document.querySelector('.options-buttons');
 const optionsWindow = document.querySelector('.options');
+
 const gridSlider = document.querySelector('.slider');
-console.log(gridSlider.value);
+let sliderValue = gridSlider.value;
+sliderValue = 16; // The dafult value for the 16x16 resolution
+const sliderValueDisplay = document.createElement('h3');
+optionsWindow.appendChild(sliderValueDisplay)
+sliderValueDisplay.textContent = gridSlider.value + 'x' + gridSlider.value;
+gridSlider.addEventListener ('input', function() {
+    sliderValueDisplay.textContent = gridSlider.value + 'x' + gridSlider.value;
+})
+
+const confirmGrid = document.createElement('button');
+optionsWindow.appendChild(confirmGrid);
+confirmGrid.classList.add('options-buttons')
+confirmGrid.textContent = 'Confirm Size'
+
 //Board and square values
-let squaresPerSide = 16; // The dafult value for the 16x16 resolution
+let squaresPerSide = sliderValue; 
 let squareSize = 600 / squaresPerSide; // 600 => Board height/width
 
 
@@ -19,4 +33,4 @@ function createGrid(squareAmount){
     }
 }
 //Default Board
-createGrid(squaresPerSide);
+createGrid(sliderValue);
