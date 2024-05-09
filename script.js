f6ac8313e5ec99aa76ba1d15dc9c3def58fbf203
Alex Bandis <bantalex3@gmail.com>
2024-05-09 17:22:59 +0300

    Make the grid function with default values

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 //DOM selectors and variables
 const board = document.querySelector('.board');
 const clearButton = document.querySelector('.clear-button');
-const optionsWindow = document.querySelector('.options')
-
-let boardSize = 256; // Default Value for 16x16
-let squareSize = 16; //Default value for 16x16
-let squareAmount = boardSize / squareSize; // Default Value for 16x16
-
-board.style.width = `${boardSize}px`;
-board.style.height = `${boardSize}px`;
-optionsWindow.style.width = `${boardSize * 1.5}px`;
+const optionsWindow = document.querySelector('.options');
+//Board and square values
+let squaresPerSide = 16; // The dafult value for the 16x16 resolution
+let squareSize = 600 / squaresPerSide; // 600 => Board height/width
 
 
-function createDeafultGrid(){
-    for (let i = 0; i < (squareSize * squareSize); i++){
+function createGrid(squareAmount){
+    for (let i = 0; i < squareAmount*squareAmount; i++){
         const square = document.createElement('div');
-        square.style.backgroundColor = 'white';
+        square.style.backgroundColor = 'var(--text)';
         square.style.width = `${squareSize}px`;
         square.style.height= `${squareSize}px`;
         board.appendChild(square);
     }
 }
-
-createDeafultGrid();
+//Default Board
+createGrid(squaresPerSide);
